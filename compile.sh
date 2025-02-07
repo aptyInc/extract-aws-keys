f500212e8eb3259061e0660861a738284fe2d993
Surya <85492089+surya-apty@users.noreply.github.com>
2025-02-07 14:50:09 +0530

    update

--- compile.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/bash
-
-set -e
-
-env GOOS=darwin go build -o ./dist/main_darwin  main.go
-env GOOS=linux go build -o ./dist/main_linux  main.go
-env GOOS=windows go build -o ./dist/main_windows.exe  main.go
--- go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/actions-go/go-action
-
-go 1.20
-
-require (
-	github.com/actions-go/toolkit v0.0.0-20231022202615-3973644be7a1
-	github.com/stretchr/testify v1.8.4
-)
